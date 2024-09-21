7f355aafe98e40af693f1498cf258de33749848e
Random Coder <random.coder@gmail.com>
2024-09-21 14:58:40 -0700

    Clean up make image helper

--- nodes_game/make_image.py
+++ nodes_game/make_image.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageOps
-import io, json, multiprocessing, os, sys, time
+import io, json, multiprocessing, os, sys
 
 def show_puzzle(data, transparent=False, solid_color=None):
     min_x = min(x[0] for x in data['points'])
@@ -32,6 +32,8 @@ def show_puzzle(data, transparent=False, solid_color=None):
     return im
 
 def enum_puzzles():
+    # Simple helper to enumerate a directory and return the results
+    # in alphabetical order
     dirs = ["data"]
     while len(dirs):
         cur_dir = dirs.pop(0)
@@ -42,17 +44,6 @@ def enum_puzzles():
             else:
                 yield fn, cur
 
-class OccasionalMessage:
-    def __init__(self, delay=0.5):
-        self.delay = delay
-        self.next_msg = time.time()
-    def __call__(self, value):
-        now = time.time()
-        if now >= self.next_msg:
-            while now >= self.next_msg:
-                self.next_msg += self.delay
-            print(value)
-
 def load_single_image(fn):
     with open(fn) as f:
         data = json.load(f)
@@ -61,169 +52,187 @@ def load_single_image(fn):
     im.save(output_fn)
     print(f"File {fn} saved as {output_fn}")
 
-def shadow_worker(cur):
-    cur['msg'] = f"Working on {cur['i']} shadow: {cur['fn']}"
-    with open(cur['fn']) as f:
-        data = json.load(f)
-    shadow = show_puzzle(data, transparent=True, solid_color=(64, 64, 64))
-    shadow.thumbnail((cur['width'], cur['height']), Image.Resampling.LANCZOS)
-    shadow = ImageOps.expand(shadow, (cur['width'], cur['height']), (0, 0, 0, 0))
-    shadow = shadow.filter(ImageFilter.BoxBlur(cur['width']//15))
-    bits = io.BytesIO()
-    shadow.save(bits, 'PNG')
-    cur['bits'] = bits.getvalue()
-    shadow.close()
-    return cur
-
 def draw_worker(cur):
-    if 'fn' in cur:
-        with open(cur['fn']) as f:
-            data = json.load(f)
-        cur['msg'] = f"Working on {cur['i']}: {cur['fn']}"
-        temp = show_puzzle(data, transparent=True)
-        temp.thumbnail((cur['width'], cur['height']), Image.Resampling.LANCZOS)
-        bits = io.BytesIO()
-        temp.save(bits, 'PNG')
-        cur['bits'] = bits.getvalue()
+    # Decode the data into an image and place it on the final image
+    with open(cur.fn) as f:
+        data = json.load(f)
+
+    temp = show_puzzle(data, transparent=True)
+    temp.thumbnail((cur.width, cur.height), Image.Resampling.LANCZOS)
+    bits = io.BytesIO()
+    temp.save(bits, 'PNG')
+    image_bits = bits.getvalue()
+    temp.close()
+
+    temp = show_puzzle(data, transparent=True, solid_color=(64, 64, 64))
+    border_x = 5 * (temp.width // cur.width)
+    border_y = 5 * (temp.height // cur.height)
+    temp = ImageOps.expand(temp, (border_x, border_y), (0, 0, 0, 0))
+    temp = temp.filter(ImageFilter.BoxBlur(cur.width * 1.5))
+    temp.thumbnail((cur.width + 5 * 2, cur.height + 5 * 2), Image.Resampling.LANCZOS)
+    bits = io.BytesIO()
+    temp.save(bits, 'PNG')
+    shadow_bits = bits.getvalue()
+    temp.close()
+
+    return cur.i, image_bits, shadow_bits
+
+class Layout:
+    def __init__(self, max_width=None, padding=0):
+        self.objects = []
+        self.row = []
+        self.padding = padding
+        self.x = padding
+        self.y = padding
+        self.max_width = max_width
+
+    def add_elem(self, obj, move_offset=True):
+        if move_offset and self.max_width is not None and len(self.row) > 0:
+            if self.x + obj.width + self.padding >= self.max_width:
+                self.new_row()
+        self.objects.append(obj)
+        self.row.append(obj)
+        obj.x = self.x
+        obj.y = self.y
+        if move_offset:
+            self.x += obj.width
+
+    def new_row(self):
+        if len(self.row) > 0:
+            self.x = self.padding
+            self.y += max(obj.height for obj in self.row)
+            self.row = []
+
+    def draw(self):
+        width = max(obj.x + obj.width + self.padding for obj in self.objects)
+        height = max(obj.y + obj.height + self.padding for obj in self.objects)
+        im = Image.new('RGB', (width, height), (255, 255, 255))
+        dr = ImageDraw.Draw(im)
+        # Draw each layer in turn
+        for layer in ["shadow", "image", "text"]:
+            for obj in self.objects:
+                if layer in obj.layers:
+                    # This object wants to be drawn on this layer, so let it draw itself
+                    obj.draw(layer, im, dr)
+        return im
+
+class ObjBase:
+    def __init__(self, width, height, layers):
+        self.x = 0
+        self.y = 0
+        self.width = width
+        self.height = height
+        self.layers = layers
+
+class ObjText(ObjBase):
+    def __init__(self, val, font_size):
+        self.val = val
+        self.fnt = ImageFont.truetype(os.path.join("..", "images", "OpenSans-Regular.ttf"), font_size)
+        bbox = self.fnt.getbbox(val)
+        super().__init__(bbox[2], int(bbox[3] * 1.1), {"text"})
+    def draw(self, layer, im, dr):
+        dr.text((self.x, self.y), self.val, (0, 0, 0), self.fnt)
+
+class ObjBlank(ObjBase):
+    def __init__(self, layers):
+        super().__init__(70, 70, layers)
+    def draw(self, layer, im, dr):
+        pass
+
+class ObjMissing(ObjBlank):
+    TARGET = None
+    def __init__(self):
+        super().__init__({"image"})
+    def draw(self, layer, im, dr):
+        if ObjMissing.TARGET is None:
+            missing_size = 1000
+            square_size = 150
+            empty = Image.new('RGBA', (missing_size, missing_size), (0, 0, 0, 0))
+            empty_draw = ImageDraw.Draw(empty)
+            for xi, x in enumerate(range(square_size // 2, missing_size - (square_size // 2), square_size)):
+                for yi, y in enumerate(range(square_size // 2, missing_size - (square_size // 2), square_size)):
+                    empty_draw.rectangle((x, y, x+square_size, y+square_size), (225, 225, 225, 255) if ((xi + yi) % 2 == 0) else (255, 255, 255, 255))
+            empty.thumbnail((self.width, self.height), Image.Resampling.LANCZOS)
+            ObjMissing.TARGET = empty
+        im.paste(ObjMissing.TARGET, (self.x, self.y), ObjMissing.TARGET)
+    
+class ObjImage(ObjBlank):
+    def __init__(self, file_only, fn):
+        super().__init__({"image", "shadow"})
+        self.file_only = file_only
+        self.fn = fn
+    def draw(self, layer, im, dr):
+        bits = io.BytesIO(self.image if (layer == "image") else self.shadow)
+        temp = Image.open(bits)
+        im.paste(temp, (self.x - (0 if (layer == "image") else 5), self.y - (0 if (layer == "image") else 5)), temp)
         temp.close()
-    return cur
 
 def main():
     if len(sys.argv) == 2:
         load_single_image(sys.argv[1])
         exit(0)
 
-    width, height = 50, 50
-    target_size = 1000
+    layout = Layout(padding=5)
 
+    # Find all the images
+    last_at = None
     images = []
-    x, y = 0, 0
-
-    fnt = ImageFont.truetype(os.path.join("..", "images", "OpenSans-Regular.ttf"), 40)
-    last_year = "--"
-
-    expected_date = None
-
     for file_only, fn in enum_puzzles():
-        # This is the "image" we're showing on the grid
-        temp = {
-            "width": width,
-            "height": height,
-            "fn": fn,
-            "file_only": file_only,
-            "year": file_only[:4],
-        }
+        at = datetime.strptime(file_only[:10], "%Y-%m-%d")
+        if last_at is not None:
+            # Add Nones for any skipped days
+            last_at += timedelta(days=1)
+            while last_at < at:
+                images.append((last_at, None, None))
+                last_at += timedelta(days=1)
+        images.append((at, file_only, fn))
+        last_at = at
 
-        # If we're starting a new year, add a placeholder for the text of the year
-        if temp['year'] != last_year:
-            if x != 0:
-                x = 0
-                y += height
-            to_disp = f"    {temp['year']}"
-            disp_size = fnt.getbbox(to_disp)
-            images.append({
-                "text": to_disp,
-                "x": x,
-                "y": y,
-                "width": int(disp_size[2]),
-                "height": int(disp_size[3] * 1.2),
-            })
-            y += images[-1]['height']
-            last_year = temp['year']
+    last_at = None
+    for at, file_only, fn in images:
+        # If we're at the start, or a new year, add a year header
+        if last_at is None or last_at.strftime("%Y") != at.strftime("%Y"):
+            layout.new_row()
+            layout.add_elem(ObjText(at.strftime("%Y"), 40))
+            layout.new_row()
+            while len(layout.row) < (at.weekday() + 1) % 7:
+                layout.add_elem(ObjBlank(set()))
 
-        # Note where we start
-        if expected_date is None:
-            expected_date = datetime(
-                year=int(file_only[0:4]),
-                month=int(file_only[5:7]),
-                day=int(file_only[8:10]),
-            )
+        # Add the placeholder image, or the real image
+        if file_only is None:
+            layout.add_elem(ObjMissing())
+        else:
+            layout.add_elem(ObjImage(file_only, fn))
 
-        # If this image skips days after the last one, add "empty" images
-        while datetime.strptime(file_only[:10], "%Y-%m-%d") > expected_date:
-            images.append({
-                "missing": True,
-                "width": width,
-                "height": height,
-                "x": x,
-                "y": y,
-            })
-            x += width
-            if x > target_size:
-                x = 0
-                y += height
-            expected_date += timedelta(days=1)
+        # If we're at a new month, then add the month info
+        # This is too noisy, try another way?
+        # if last_at is None or last_at.strftime("%b") != at.strftime("%b"):
+        #     obj = ObjText(at.strftime("%b"), 20)
+        #     layout.add_elem(obj, move_offset=False)
+        #     obj.x = layout.objects[-2].x
+        #     obj.y = layout.objects[-2].y
 
-        # Finally, add the image to the list of things to do
-        temp["x"] = x
-        temp["y"] = y
-        images.append(temp)
+        # If we've shown 14 images, then go ahead and move to a new row
+        if sum(1 for x in layout.row if issubclass(type(x), ObjBlank)) >= 14:
+            layout.new_row()
 
-        # Update the date we expect to see
-        expected_date += timedelta(days=1)
-        # And move to the next spot in the grid
-        x += width
-        if x > target_size:
-            x = 0
-            y += height
+        last_at = at
 
-    # Draw the placeholder image
-    missing_size = 1000
-    square_size = 150
-    empty = Image.new('RGBA', (missing_size, missing_size), (0, 0, 0, 0))
-    empty_draw = ImageDraw.Draw(empty)
-    for xi, x in enumerate(range(square_size // 2, missing_size - (square_size // 2), square_size)):
-        for yi, y in enumerate(range(square_size // 2, missing_size - (square_size // 2), square_size)):
-            empty_draw.rectangle((x, y, x+square_size, y+square_size), (225, 225, 225, 255) if (xi + yi) % 2 == 0 else (255, 255, 255, 255))
-    empty.thumbnail((width, height), Image.Resampling.LANCZOS)
+    # Prepare the data for all of the images
+    todo = []
+    for i, cur in enumerate(layout.objects):
+        if isinstance(cur, ObjImage):
+            cur.i = i
+            todo.append(cur)
 
-    # Figure out how big the final image needs to be
-    width = max((x['width'] + x['x']) for x in images)
-    height = max((x['height'] + x['y']) for x in images)
-
-    # Add some padding and move evertyhing over a bit
-    width += 20
-    height += 20
-    for cur in images:
-        cur['x'] += 10
-        cur['y'] += 10
-
-    im = Image.new('RGB', (width, height), (255, 255, 255))
-    dr = ImageDraw.Draw(im)
-    last_year = "--"
-
-    occasional = OccasionalMessage()
-
-    for i, cur in enumerate(images):
-        cur['i'] = i
-
+    # Draw everything in a pool to speed it up a bit    
     with multiprocessing.Pool() as pool:
-        # Run through and draw drop shadows for all of the images
-        for cur in pool.imap(shadow_worker, [x for x in images if 'fn' in x]):
-            occasional(cur['msg'])
-            bits = io.BytesIO(cur['bits'])
-            shadow = Image.open(bits)
-            im.paste(shadow, (cur['x']-cur['width']+(cur['width']//25), cur['y']-cur['height']+(cur['height']//25)), shadow)
-            shadow.close()
+        for i, image, shadow in pool.imap_unordered(draw_worker, todo):
+            layout.objects[i].image = image
+            layout.objects[i].shadow = shadow
 
-        # And create the images themselves
-        for cur in pool.imap(draw_worker, images):
-            if 'missing' in cur:
-                # Just place the "empty" image
-                im.paste(empty, (cur['x'], cur['y']), empty)
-            elif 'text' in cur:
-                # Draw the year
-                dr.text((cur['x'], cur['y']), cur['text'], (0, 0, 0), fnt)
-            elif 'fn' in cur:
-                # Draw the decoded image
-                occasional(cur['msg'])
-                bits = io.BytesIO(cur['bits'])
-                temp = Image.open(bits)
-                im.paste(temp, (cur['x'], cur['y']), temp)
-                temp.close()
-            else:
-                raise Exception()
-
+    # And finally draw all the things
+    im = layout.draw()
     im.save(os.path.join("images", "preview.png"))
     print("Done!")
 
